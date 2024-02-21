e6b1b5bc25c7dd47d6dd462b43b6ec0cc17b9f3e
Jarrod Brown <124294296+goodguyguu@users.noreply.github.com>
2024-02-20 20:53:57 -0500

    Delete Week 6/RecipesApp directory

--- Week 6/RecipesApp/components/NavButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from 'react-native';
-
-const NavButton = ({ title, onPress }) => (
-  <Button title={title} onPress={onPress} />
-);
-
-export default NavButton;
